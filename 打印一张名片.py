f5ab112ff311d62ce572fa00f0ad3d5b601c58d8
徐志林 <38243028+adorm@users.noreply.github.com>
2018-06-04 22:15:48 +0800

    Add files via upload

---
+++ 打印一张名片.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+# 用input获取必要的信息
+
+name = input("请输入你的名字")
+QQ = input("请输入你的QQ号")
+
+# 使用print打印输出
+
+print("==============")
+print("姓名：%s"%name)    # 打印输出姓名
+print("QQ:%s"%QQ)         # 打印输出QQ号
+print("==============")
