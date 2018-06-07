cb5893c6013d78f954f2bf83e2695b330db94460
徐志林 <38243028+adorm@users.noreply.github.com>
2018-06-07 21:22:07 +0800

    Add files via upload

---
+++ 定义变量.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+score = 100                      # 定义了一个变量，给变量附了一个值
+
+high = 200                       # 变量名，并赋值
+
+applePrice = 3.5                 # 苹果的价格
+
+weight = 7.5                     # 苹果的重量
+
+money = applePrice * weight      # 总价钱等于money 苹果的价钱乘以苹果的重量
+
+money = money - 10               # 给这个变量一个新值
